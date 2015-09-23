70dfa3767d734a9d542af9ace7bc5246e85793ee
tsantos <tales.augusto.santos@gmail.com>
2015-09-22 22:30:06 -0300

    Added favicon

--- app/views/base.html.php
+++ app/views/base.html.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8" />
         <title><?php $view['slots']->output('title', 'Welcome!') ?></title>
         <?php $view['slots']->output('stylesheet') ?>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     </head>
     <body>
         <?php $view['slots']->output('_content') ?>
